8302c198c34dd9d8b1914a3ac89a66a4b097d080
v144 <v144@MacBook-Pro-v144.local>
2016-03-03 02:32:54 +0300

    Improved realization of class levelStorage, namely I finished method getLevel and and in this method was defined physics of nodes.In addition test level(it is called 'Level0.csb') was created and added to project. To use it, call 'getLevel(0)'.

--- proj.ios_mac/build/SPH.build/Debug-iphonesimulator/SPH-mobile.build/Objects-normal/x86_64/LevelStorage.d
+++ proj.ios_mac/build/SPH.build/Debug-iphonesimulator/SPH-mobile.build/Objects-normal/x86_64/LevelStorage.d
@@ -1,3 +1,3 @@
 dependencies: \
-  /Users/v144/Documents/CocosProjects/SPH/proj.ios_mac/LevelStorage.cpp \
-  /Users/v144/Documents/CocosProjects/SPH/proj.ios_mac/LevelStorage.hpp
+  /Users/v144/Documents/CocosProjects/SPH/Classes/LevelStorage.cpp \
+  /Users/v144/Documents/CocosProjects/SPH/Classes/LevelStorage.hpp
